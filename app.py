556f229651858b01a96c77af4193be9ad5c05f3a
Gabriele-Frattini <96744665+Gabriele-Frattini@users.noreply.github.com>
2022-02-06 10:20:02 +0100

    added questions to app

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 
 connection = sqlite3.connect("data.sqlite")
 
+    # connecting to sqllite and appending data to list
 lists = []
 with connection as conn:
     exc = conn.execute(
@@ -13,9 +14,11 @@ with connection as conn:
     for i in exc.fetchall():
         lists.append(i)
 
+        # making it in to a dataframe
 df = pd.DataFrame(
     lists, columns=["monthly_price", "start", "end"]).reset_index()
 
+# creating a df for all months with subscriptions
 new_dates = []
 df2 = pd.DataFrame()
 lists = []
@@ -41,6 +44,8 @@ for index, i in enumerate(df.start):
 
 df_dates = pd.DataFrame(new_dates, columns=["date", "index"])
 
+# merging and grouping df with revenue and dates
+
 df_all = df.merge(df_dates, left_on=df["index"], right_on=df_dates["index"])
 df_all = df_all.drop(["key_0", "index_y"], axis=1)
 df_all["monthly_price"] = pd.to_numeric(df_all.monthly_price)
@@ -54,9 +59,12 @@ years = [str(i.year) for i in price_months.date]
 
 price_months = pd.concat([price_months, pd.Series(years, name="year")], axis=1)
 
+#plot for task 1
+
 plot_task1 = px.line(price_months.sort_values("date"), x="month", y="monthly_price", color="year",
         title="Revenue Generated for 2020 & 2021")
 
+# sql query for task 2
 
 monthly_subscriptions = []
 with connection as conn:
@@ -65,16 +73,8 @@ with connection as conn:
     for i in exc.fetchall():
         monthly_subscriptions.append(i)
 
-y = pd.DataFrame(lists, columns=["revenue", "date"])
-y["date"] = pd.to_datetime(y.date)
-y["revenue"] = pd.to_numeric(y.revenue)
-
-y = y.sort_values("date")
-px.line(y, x="date", y="revenue",
-        title="Revenue $ Generated from new subscriptions")
-
-
-
+    # queries for task 2
+ 
 monthly_subscriptions = []
 with connection as conn:
     exc = conn.execute(
@@ -82,6 +82,8 @@ with connection as conn:
     for i in exc.fetchall():
         monthly_subscriptions.append(i)
 
+        #adding monthly revenue and date to df
+        
 df_revenue = pd.DataFrame(monthly_subscriptions, columns=["revenue", "date"])
 df_revenue["date"] = pd.to_datetime(df_revenue.date)
 df_revenue["revenue"] = pd.to_numeric(df_revenue.revenue)
@@ -90,7 +92,13 @@ df_revenue = df_revenue.sort_values("date")
 plot_task2 = px.line(df_revenue, x="date", y="revenue",
         title="Revenue $ Generated from new subscriptions")
 
+
+    # streamlit wepage
+
 st.title("Task submission for internship at CARIFY")
+
+st.subheader("What was the total revenues generated in the year 2020 and 2021, respectively?")
+
 st.plotly_chart(plot_task1)
 
 with connection as conn:
@@ -110,9 +118,14 @@ with connection as conn:
     for i in exc2.fetchall():
         revenue_20 = i[0]
         revenue_21 = i[1]
-        
-click_20 = st.button("Revenue generated for 2020")  
-click_21 = st.button("Revenue generated for 2021")
+
+col1, col2 = st.beta_columns(2)
+
+with col1:
+    click_20 = st.button("Revenue generated for 2020")
+
+with col2:
+    click_21 = st.button("Revenue generated for 2021")
 
 if click_20:
     st.subheader(revenue_20)
@@ -125,7 +138,9 @@ with connection as conn:
         "SELECT CAST(SUM(subscription_monthly_price) AS INT) AS total, strftime('%Y-%m',start_date) AS month FROM task GROUP BY month ORDER BY total DESC LIMIT 1")
 
     fetch = exc2.fetchone()
-    
+
+st.subheader("Identify the month with the highest amount of revenues arising from new subscriptions")
+
 st.plotly_chart(plot_task2)
 button = st.button("Month with the highest amount of revenue arising from new subscriptions")
 
